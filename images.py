5bfc87fa188cac5a1ae3ac8183884a80fc1a8c64
Allex Radu <eusceptic@gmail.com>
2021-02-24 18:17:53 +0200

    adding images.py and kv_multiple_values_no_type

---
+++ images.py
@@ -0,0 +1,40 @@
+import openpyxl as xl
+import platform
+import json
+
+table_location = 'excel\\a.xlsx' if platform.system() == 'Windows' else 'excel/a.xlsx'
+wb = xl.load_workbook(table_location)
+sh = wb[wb.sheetnames[0]]
+build_table_location = 'excel\\a_b.xlsx' if platform.system() == 'Windows' else 'excel/a_b.xlsx'
+build_wb = xl.load_workbook(build_table_location)
+build_sh = build_wb[build_wb.sheetnames[0]]
+data = {}
+col_dict = {}
+current_new_col = 2
+row_index = 2
+
+for row in range(2, sh.max_row + 1):
+    print('row: ', row)
+    code = sh.cell(row, 1).value
+    key = sh.cell(row, 2).value
+
+    if code not in data.keys():
+        data.update({code: [key]})
+    else:
+        data[code].append(key)
+
+with open('data.json', 'w') as fp:
+    json.dump(data, fp, indent = 4)
+
+for code in data.keys():
+    build_sh.cell(row_index, 1).value = code
+    print('build row', row_index)
+
+    for a_type in data.keys():
+        keys = data[code]
+        for i in range(len(keys)):
+            build_sh.cell(row_index, i + 2).value = keys[i]
+
+    row_index += 1
+
+build_wb.save(build_table_location)
